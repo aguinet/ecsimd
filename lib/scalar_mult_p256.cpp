eb90ddab388b85175735d397af7c1d8b3253ae9a
Adrien Guinet <adrien@guinet.me>
2022-01-09 18:42:29 +0100

    cleanup

--- lib/scalar_mult_p256.cpp
+++ lib/scalar_mult_p256.cpp
@@ -4,7 +4,6 @@
 using namespace ecsimd;
 
 using Curve = curve_nist_p256;
-using CurveGroup = curve_group<Curve>;
 using WBN = curve_wide_bn_t<Curve>;
 using WJCP = wide_jacobian_curve_point<Curve>;
 
