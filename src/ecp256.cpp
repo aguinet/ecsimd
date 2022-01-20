2a9877db2b6290ee9b102ad57567298eb4970c26
Adrien Guinet <adrien@guinet.me>
2022-01-20 09:32:46 +0100

    working with classical mod

--- src/ecp256.cpp
+++ src/ecp256.cpp
@@ -8,6 +8,7 @@
 #include <ctbignum/montgomery.hpp>
 #include <ctbignum/io.hpp>
 #include <ctbignum/mod_inv.hpp>
+#include <ctbignum/invariant_div.hpp>
 
 #include <benchmark/benchmark.h>
 
@@ -42,64 +43,40 @@ constexpr cbn_u512 to_cbn_512(u512 const& v) {
 
 constexpr auto P = 115792089210356248762697446949407573530086143415290314195533631308867097853951_Z;
 constexpr auto cbn_P = cbn::to_big_int(P);
-constexpr auto R   = cbn::detail::unary_encoding<nlimbs, nlimbs + 1, limb_type>();
-constexpr auto Rsq = cbn::detail::unary_encoding<2 * nlimbs, 2 * nlimbs + 1, limb_type>();
-constexpr auto R_p   = cbn::div(R, P).remainder;
-constexpr auto Rsq_p = cbn::div(Rsq, P).remainder;
-constexpr auto Pm1_by_R_p = cbn::div((cbn_P-cbn_u256{1})*R, P).remainder;
 
-constexpr cbn_u256 mgry_reduce(cbn_u512 const& n) {
-  return cbn::montgomery_reduction(n, P);
+cbn_u256 fast_reduce(cbn_u512 const& n) {
+  return cbn::mod(n, P);
 }
 
-constexpr cbn_u256 mgry_mul(cbn_u256 const& a, cbn_u256 const& b) {
-  return mgry_reduce(cbn::mul(a,b));
-  //return cbn::montgomery_mul(a,b,P);
+cbn_u256 mul_mod(cbn_u256 const& a, cbn_u256 const& b) {
+  const auto m = cbn::mul(a,b);
+  return fast_reduce(m);
 }
 
-constexpr cbn_u256 mgry_sqr(cbn_u256 const& a) {
-  return mgry_reduce(cbn::mul(a,a));
-  //return cbn::montgomery_mul(a,a,P);
-  //return mgry_reduce(cbn::mul(a,a));
+cbn_u256 sqr_mod(cbn_u256 const& a) {
+  return mul_mod(a,a);
 }
 
-cbn_u256 mgry_add(cbn_u256 const& a, cbn_u256 const& b) {
-  return cbn::mod_add(a,b,cbn_P);
-}
+struct gfp {
+  gfp() = default;
 
-cbn_u256 mgry_sub(cbn_u256 const& a, cbn_u256 const& b) {
-  return cbn::mod_sub(a,b,cbn_P);
-}
-
-struct mgry {
-  mgry() = default;
-
-  constexpr mgry(cbn_u256 const& n):
+  constexpr gfp(cbn_u256 const& n):
     n_(n)
   { }
 
-  static constexpr mgry from_classical(cbn_u256 const& n) {
-    return mgry{mgry_mul(n, Rsq_p)};
+  gfp sqr() const {
+    return gfp{sqr_mod(n_)};
   }
 
-  mgry sqr() const {
-    return mgry_sqr(n_);
-  }
-
-  cbn_u256 to_classical() const {
-    const cbn_u512 wn = cbn::detail::pad<nlimbs>(n_);
-    return mgry_reduce(wn);
-  }
-
-  mgry opposite() const {
-    auto ret = mgry_sub(n_, R_p);
-    return mgry{mgry_sub(Pm1_by_R_p, ret)};
+  gfp opposite() const {
+    assert(n_ < cbn_P);
+    return gfp{cbn::subtract_ignore_carry(cbn_P,n_)};
   }
 
   cbn_u256 const& bn() const { return n_; }
   cbn_u256& bn() { return n_; }
 
-  auto operator<=>(mgry const& o) const {
+  auto operator<=>(gfp const& o) const {
     return bn() <=> o.bn();
   }
 
@@ -107,22 +84,22 @@ private:
   cbn_u256 n_;
 };
 
-mgry operator*(mgry const& a, mgry const& b) {
-  return mgry{mgry_mul(a.bn(), b.bn())};
+gfp operator*(gfp const& a, gfp const& b) {
+  return gfp{mul_mod(a.bn(), b.bn())};
 }
 
-mgry operator+(mgry const& a, mgry const& b) {
-  return mgry{mgry_add(a.bn(), b.bn())};
+gfp operator+(gfp const& a, gfp const& b) {
+  return gfp{cbn::mod_add(a.bn(), b.bn(), cbn_P)};
 }
 
-mgry operator-(mgry const& a, mgry const& b) {
-  return mgry{mgry_sub(a.bn(), b.bn())};
+gfp operator-(gfp const& a, gfp const& b) {
+  return gfp{cbn::mod_sub(a.bn(), b.bn(), cbn_P)};
 }
 
 template <size_t Count>
-mgry mgry_shift_left(mgry const& a) {
+gfp gfp_shift_left(gfp const& a) {
   // TODO: optimize
-  mgry ret = a;
+  gfp ret = a;
 #pragma unroll
   for (size_t i = 0; i < Count; ++i) {
     ret = ret+ret;
@@ -153,22 +130,22 @@ private:
 struct jacobian_curve_point {
   static jacobian_curve_point from_affine(curve_point const& pt) {
     jacobian_curve_point ret;
-    ret.x_ = mgry::from_classical(pt.x());
-    ret.y_ = mgry::from_classical(pt.y());
-    ret.z_ = mgry{R_p};
+    ret.x_ = gfp{pt.x()};
+    ret.y_ = gfp{pt.y()};
+    ret.z_ = gfp{cbn_u256{1,0,0,0}};
     return ret;
   }
 
   curve_point to_affine() const {
     // Unoptimized, only for testing purposes
-    const auto Z = z_.to_classical();
+    const auto& Z = z_.bn();
     const auto invZ = cbn::mod_inv(Z, cbn_P);
-    const auto invZ2 = (invZ*invZ)%cbn_P;
-    const auto invZ3 = (invZ2*invZ)%cbn_P;
+    const auto invZ2 = sqr_mod(invZ);
+    const auto invZ3 = mul_mod(invZ2,invZ);
 
     curve_point ret;
-    ret.x() = (x_.to_classical() * invZ2)%cbn_P;
-    ret.y() = (y_.to_classical() * invZ3)%cbn_P;
+    ret.x() = mul_mod(x_.bn(), invZ2);
+    ret.y() = mul_mod(y_.bn(), invZ3);
     return ret;
   }
 
@@ -190,15 +167,15 @@ struct jacobian_curve_point {
 
 
 private:
-  mgry x_;
-  mgry y_;
-  mgry z_;
+  gfp x_;
+  gfp y_;
+  gfp z_;
 };
 
 using JCP = jacobian_curve_point;
 
-// mgry(-3, p)
-constexpr mgry Am = mgry::from_classical(cbn::to_big_int(115792089210356248762697446949407573530086143415290314195533631308867097853948_Z));
+// -3 % p
+constexpr gfp Am = gfp{cbn::to_big_int(115792089210356248762697446949407573530086143415290314195533631308867097853948_Z)};
 
 // Double-with-update (co-Z). P.z must be equal to mgry(1).
 JCP DBLU(JCP& P) {
@@ -209,14 +186,14 @@ JCP DBLU(JCP& P) {
   const auto B = X1.sqr();
   const auto E = Y1.sqr();
   const auto L = E.sqr();
-  const auto S = mgry_shift_left<1>((X1 + E).sqr() - B - L);
-  const auto M = (mgry_shift_left<1>(B)+B) + Am;
+  const auto S = gfp_shift_left<1>((X1 + E).sqr() - B - L);
+  const auto M = (gfp_shift_left<1>(B)+B) + Am;
 
   JCP ret;
-  ret.x() = M.sqr() - mgry_shift_left<1>(S);
-  const auto Lm8 = mgry_shift_left<3>(L);
+  ret.x() = M.sqr() - gfp_shift_left<1>(S);
+  const auto Lm8 = gfp_shift_left<3>(L);
   ret.y() = M*(S-ret.x()) - Lm8;
-  ret.z() = mgry_shift_left<1>(Y1);
+  ret.z() = gfp_shift_left<1>(Y1);
 
   // Update P
   X1 = S;
@@ -273,20 +250,20 @@ JCP ZDAU(JCP const& P, JCP& Q) {
   const auto A1p = Y1*(W1p-W2p);
   const auto X3pc = Dp - W1p - W2p;
   const auto C = (X3pc - W1p).sqr();
-  const auto Y3p = ((Y1-Y2) + (W1p - X3pc)).sqr() - Dp - C - mgry_shift_left<1>(A1p);
-  const auto W1 = mgry_shift_left<2>(X3pc)*C;
-  const auto W2 = mgry_shift_left<2>(W1p)*C;
-  const auto D = (Y3p-mgry_shift_left<1>(A1p)).sqr();
+  const auto Y3p = ((Y1-Y2) + (W1p - X3pc)).sqr() - Dp - C - gfp_shift_left<1>(A1p);
+  const auto W1 = gfp_shift_left<2>(X3pc)*C;
+  const auto W2 = gfp_shift_left<2>(W1p)*C;
+  const auto D = (Y3p-gfp_shift_left<1>(A1p)).sqr();
   const auto A1 = Y3p*(W1-W2);
 
   JCP ret;
   ret.x() = D-W1-W2;
-  ret.y() = (Y3p-mgry_shift_left<1>(A1p))*(W1-ret.x()) - A1;
+  ret.y() = (Y3p-gfp_shift_left<1>(A1p))*(W1-ret.x()) - A1;
   ret.z() = Z*((X1-X2+X3pc-W1p).sqr() - Cp - C);
 
-  const auto Dc = (Y3p+mgry_shift_left<1>(A1p)).sqr();
+  const auto Dc = (Y3p+gfp_shift_left<1>(A1p)).sqr();
   X2 = Dc - W1 - W2;
-  Y2 = (Y3p + mgry_shift_left<1>(A1p))*(W1-X2)-A1;
+  Y2 = (Y3p + gfp_shift_left<1>(A1p))*(W1-X2)-A1;
   Q.z() = ret.z();
 
   return ret;
@@ -305,14 +282,14 @@ JCP ADD_Z2_1(JCP const& A, JCP const& B) {
   const auto S2 = Y2*Z1*Z1Z1;
   const auto H = U2-X1;
   const auto HH = H.sqr();
-  const auto I = mgry_shift_left<2>(HH);
+  const auto I = gfp_shift_left<2>(HH);
   const auto J = H*I;
-  const auto r = mgry_shift_left<1>(S2-Y1);
+  const auto r = gfp_shift_left<1>(S2-Y1);
   const auto V = X1*I;
 
   JCP ret;
-  ret.x() = r.sqr()-J-mgry_shift_left<1>(V);
-  ret.y() = r*(V-ret.x())-mgry_shift_left<1>(Y1)*J;
+  ret.x() = r.sqr()-J-gfp_shift_left<1>(V);
+  ret.y() = r*(V-ret.x())-gfp_shift_left<1>(Y1)*J;
   ret.z() = (Z1+H).sqr()-Z1Z1-HH;
 
   return ret;
@@ -386,25 +363,12 @@ void bench_p256(benchmark::State& S) {
 
 int main(int argc, char** argv)
 {
-  const cbn_u256 a = rnd_u256();
-  const cbn_u256 b = rnd_u256();
-
-  const auto ma = mgry::from_classical(a);
-  const auto mb = mgry::from_classical(b);
-
-  const auto mref = cbn::mul(a,b)%cbn_P;
-  const auto mmul = ma*mb;
-  const cbn_u256 mul = mmul.to_classical();
-
-  std::cout << (mref == mul) << std::endl;
-  std::cout << ((ma+mb).to_classical() == cbn::mod_add(a,b,cbn_P)) << std::endl;
-  std::cout << ((ma-mb).to_classical() == cbn::mod_sub(a,b,cbn_P)) << std::endl;
-
   const cbn_u256 Gx = cbn::to_big_int(48439561293906451759052585252797914202762949526041747995844080717082404635286_Z);
   const cbn_u256 Gy = cbn::to_big_int(36134250956749795798585127919587881956611106672985015071877198253568414405109_Z);
   const curve_point G(Gx,Gy);
   const auto JG = jacobian_curve_point::from_affine(G);
 
+  const auto a = rnd_u256();
   const auto P = scalar_mult(a, JG).to_affine();
   std::cout << "a=" << a << std::endl;
   std::cout << "x=" << P.x() << std::endl;
